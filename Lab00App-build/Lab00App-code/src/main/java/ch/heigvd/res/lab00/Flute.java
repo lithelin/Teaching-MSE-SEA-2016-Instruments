f0557a20118bd90bf9acaf29318a0f77ee7cf469
J. Michielin <julie.michielin@master.hes-so.ch>
2016-04-21 15:25:34 +0200

    [Lab00] Dev: added Flute implement interface

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,38 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * This is a very simple class used to demonstrate the specify-implement-validate
+ * cycle. All methods used in the JUnit test are defined, so the test class will
+ * compile. There is a bug in the add method, so one of the tests is expected
+ * to fail.
+ * 
+ * If you look at the class named ch.heigvd.res.lab01.ApplicationTest, you will
+ * find an executable specification for this class. The test methods specify
+ * the expected behavior for this class.
+ * 
+ * @author Olivier Liechti
+ */
+public class Flute implements IInstrument {
+
+  private int soundVolume;
+  private String color = "blue";
+  private String sound = "tttttuuuuuu";
+
+  public Trumpet() {
+    super();
+  }
+
+
+  public Trumpet(String message) {
+    this.message = message;
+  }
+
+  public String getColor(){
+    return color;
+  }
+
+  public String getSound() {
+    return sound;
+  }
+
+}
